707600b46445378473df687acde4c4d20e0588b8
Irwansyah <109840077+iwplus@users.noreply.github.com>
2023-05-31 05:19:54 +0800

    Update mixedwithPSO.R

--- mixedwithPSO.R
+++ mixedwithPSO.R
@@ -1,22 +1,22 @@
 ##############################################################
-###### Library/Packages yang diperlukan untuk run program ###########
+###### Library/Packages needed ###########
 ##############################################################
 
 library(pracma)
 
 ##############################################################
-############ Fungsi tambahan #################################
+############ additional functions #################################
 ##############################################################
 
 
 
-############## Fungsi menghapus unsur nol di vektor #######
+############## Function to delete zero entries in an array -- needed for reading the indices for independet variables #######
 
 removezero = function(a)
 {
  indzero = 0
  ind = 1
- while (ind <= length(a)) #### cari indeks entri tak-nol pertama
+ while (ind <= length(a)) 
  {
 	if (a[ind] != 0) 
 	{
@@ -26,7 +26,7 @@ removezero = function(a)
  ind = ind + 1
  }
  
- for (i in (indzero+1):length(a)) ### cari semua indeks entri tak-nol
+ for (i in (indzero+1):length(a)) 
  {
 	if (a[i] != 0)
 	{
@@ -48,22 +48,22 @@ removezero = function(a)
 
 
 ###########################################
-### (1) Bagian particle swarm optimization ####
+### (1) Particle swarm optimization (PSO) ####
 ###########################################
 
 ###############################################
-####Daftar fungsi-fungsi yang berguna #########
+#### some useful functions #########
 ###############################################
 
 
-### fungsi bobot inersia ####
+### Innertia weight function ####
 
 omega <- function(t,tmaks){
 	bobinersia = 0.9-(((0.9-0.4)/tmaks)*t);
 	bobinersia
 }
 
-### fungsi pembangkit matriks R1 dan R2 ##########
+### A function to generate R1 and R2 ##########
 
 generater12 <- function(nvar){
  entridiag = c(matrix(runif(1*nvar), ncol = nvar));
@@ -81,24 +81,24 @@ generater12 <- function(nvar){
 
 
 #########################################
-#### Inisialisasi input PSO* ############
+#### An example for PSO input initialization ############
 #########################################
-### *sebagai contoh saja (di masing-masing bagian sudah ditaruh inisialisasi juga) ################
 
-#N = 5; ### Banyaknya calon solusi awal
-#itermaks = 100; ### banyaknya iterasi maksimum
-#nvar = 5; ### diisi dengan banyaknya variabel 
+
+#N = 5; ### The number of initial solutions
+#itermaks = 100; ### number of iterations
+#nvar = 5; ### number of variables (dependent + independent) 
 
 
 ################################################
-############ Fungsi PSO ########################
+############ PSO main function ########################
 ################################################
 
  
-pso <- function(N,itermax,nvar,f,y,x,batasvar,Dalpha,degree,nknot) #### bagian awal fungsi pso yang meminimumkan f
+pso <- function(N,itermax,nvar,f,y,x,batasvar,Dalpha,degree,nknot) #### minimize f
 {
  npartisidomainvar = 100
- X = matrix(0, nrow=N, ncol = nvar*nknot); ### bangkitkan solusi awal
+ X = matrix(0, nrow=N, ncol = nvar*nknot); ### generate initial solutions
  for (i in 1:N)
  {
    for (j in 1:nvar)
@@ -115,8 +115,8 @@ pso <- function(N,itermax,nvar,f,y,x,batasvar,Dalpha,degree,nknot) #### bagian a
 
  #print(X)
 
- p = X; ### personal best awal
- v = matrix(0,N,nvar*nknot); ### kecepatan awal
+ p = X; ### initial personal best
+ v = matrix(0,N,nvar*nknot); ### initial velocity
 
 
  fitness = matrix(0,1,N);
@@ -129,7 +129,7 @@ pso <- function(N,itermax,nvar,f,y,x,batasvar,Dalpha,degree,nknot) #### bagian a
 
  sortfitness = sort(fitness, index.return = TRUE);
 
- g = X[sortfitness$ix[1],]; ### solusi minimum
+ g = X[sortfitness$ix[1],]; ### best solution so far
 
  #print(fitness)
  #print(X)
@@ -137,14 +137,14 @@ pso <- function(N,itermax,nvar,f,y,x,batasvar,Dalpha,degree,nknot) #### bagian a
  for (iter in 2:itermaks){
 
 
- ### update kecepatan particle ###
+ ### particle velocity update  ###
 
  for (i in 1:N){
  	v[i,] = (omega(iter,itermaks)*v[i,])+(2*(p[i,]-X[i,])%*%generater12(nvar*nknot))+(2*(g-X[i,])%*%generater12(nvar*nknot));
  }
 
 
- ##### update solusi #####
+ ##### solution update #####
 
  for (i in 1:N){
 	X[i,] = X[i,] + v[i,];
@@ -167,9 +167,9 @@ pso <- function(N,itermax,nvar,f,y,x,batasvar,Dalpha,degree,nknot) #### bagian a
 
  c(g,f(y,x,g,Dalpha,degree))
 
-} ######################################### akhir dari fungsi pso
+} ######################################### the end of pso pso function
 
-#output = pso(N,itermaks,nvar,kuadratik,Dalpha,degree);
+#output = pso(N,itermaks,nvar,kuadratik,Dalpha,degree);  ### how to use PSO main function
 
 #########################################################
 #########################################################
@@ -178,21 +178,21 @@ pso <- function(N,itermax,nvar,f,y,x,batasvar,Dalpha,degree,nknot) #### bagian a
 
 
 #####################################################
-############ (2) Bagian Regresi Kernel ##################
+############ (2) Non-parametric kernel regression ##################
 #####################################################
 
 #########################################
-##### fungsi kernel dan fungsi GCV#######
+##### kernel function and Generalized Cross Validation (GCV) function#######
 #########################################
 
-kernel = function(u) #### kernel bi-square
+kernel = function(u) #### bi-square kernel 
 {
  if (abs(u)<=1) {ker = 0.9375*(1-u^2)^2}
  else {ker = 0}
  return(ker)
 }
 
-euclidnorm = function(b) #### Norma Euclid 
+euclidnorm = function(b) #### Euclidean distance 
 {
  norma = 0
  for (i in 1:length(b))
@@ -203,7 +203,7 @@ euclidnorm = function(b) #### Norma Euclid
 }#######################################
 
 
-nadwatson = function(x0,x1,x,h) ###### Fungsi W pada estimator Nadaraya-Watson
+nadwatson = function(x0,x1,x,h) ###### Function W in Nadaraya-Watson estimator
 {
  m = length(x)
  K = 0
@@ -223,17 +223,17 @@ nadwatson = function(x0,x1,x,h) ###### Fungsi W pada estimator Nadaraya-Watson
 }################################################################################
 
 #########################################################################
-##################### Fungsi GCV* #######################################
+##################### GCV main function* #######################################
 #########################################################################
-########## * keterangan : parameter Dalpha dan degree diset fiks. Parameter tsb Ditambahkan hanya untuk keseragaman input fungsi di PSO ########
+########## *Note : the variables Dalpha and degree fixed. added to make 'appropriate' input for PSO main function ########
 #########################################################################
 
-gcv = function(y,x,h,Dalpha,degree)############### Fungsi GCV yang univariat
+gcv = function(y,x,h,Dalpha,degree)############### GCV for univariate case
 {
  n = length(y)
  D = matrix(0, ncol = n, nrow = n) ### Inisialisasi matriks D(h)
  
- for (k in 1:n) ##### pengisian entri matriks D(h) dengan fungsi W dari estimator Nadaraya-Watson
+ for (k in 1:n) ##### D(h) matrix entries with function W of Nadaraya-Watson estimator 
  {
    for (l in 1:n)
    {
@@ -247,15 +247,15 @@ gcv = function(y,x,h,Dalpha,degree)############### Fungsi GCV yang univariat
  gcvvalue = (1/n*euclidnorm(HB)^2)/((1/n*sum(diag(HA)))^2)
  
  return(gcvvalue)
-} ##################################################### Akhir fungsi GCV 
+} ##################################################### the end of GCV main function 
 ########################################################################################################
 
 
 #####################################################################################
-############# (3) Bagian Spline Truncated ###############################################
+############# (3) Spline Truncated ###############################################
 #####################################################################################
 
-#### Fungsi truncated ############
+#### truncated function ############
 
 truncate = function(t,k,d)
 {
@@ -271,7 +271,7 @@ truncate = function(t,k,d)
 }
 
 ################################################################################
-################### Fungsi GCV untuk Mix* ######################################
+################### GCV function for Mixed non-parametric regression* ######################################
 ################################################################################
 
 
@@ -281,9 +281,9 @@ gcvmix = function(y,t,knot,Dalpha,degree)
  nmat = ncol(t)
  nknot = length(knot)/nmat
  m = degree + 1 + nknot
- Gk = matrix(0,nrow = n, ncol = m) ###### inisialisasi matriks G(k)
+ Gk = matrix(0,nrow = n, ncol = m) 
  
- for (indcol in 1:m) ###### menghitung G(k_1)
+ for (indcol in 1:m) 
  {
 	if (indcol <= degree+1)
 	{
@@ -301,10 +301,10 @@ gcvmix = function(y,t,knot,Dalpha,degree)
 	}
  }	
  
- for (indvar in 2:nmat) #### menghitung G(k_2),...,G(k_p) dan digabung jadi G(k)
+ for (indvar in 2:nmat) 
  {
 	Gtemp = matrix(0, nrow = n, ncol = m)
-	for (indcol in 1:m) ###### menghitung G(k_1)
+	for (indcol in 1:m) 
  	{
 		if (indcol <= (degree+1))
 		{
@@ -340,28 +340,28 @@ gcvmix = function(y,t,knot,Dalpha,degree)
 
 
 ################################################################
-####### (4) Coba Pengolahan data (eksekusi) #############
+####### (4) Computation #############
 ################################################################
 
-##### Baca data dan parameter lain yang diperlukan #############
+##### Read the data and indices for kernel and spline truncated independent variables #############
 
-mydata = read.csv('e:2019/PSO plus Mixed Spline and Kernel/datacoba.csv',header=TRUE)
-dataindeks = read.csv('e:2019/PSO plus Mixed Spline and Kernel/indeks.csv',header=TRUE)
+mydata = read.csv('datacoba.csv',header=TRUE)
+dataindeks = read.csv('indeks.csv',header=TRUE)
 
 
-####### Data (variabel) respon/dependent ###########
+####### dependent variable ###########
 
 mydata = data.frame(mydata)
 y = as.matrix(mydata[,3])
 
-### Input indeks variabel yang sudah tercatat di indeks.csv####
+### Independent variables ####
 
 dataindeks = data.frame(dataindeks)
 indeks = as.matrix(dataindeks)
 
-##### Data (variabel) independent ########################
 
-indeksx = removezero(indeks[,1]) ##### indeks variabel x ada di kolom ke-1 di file indeks.csv 
+
+indeksx = removezero(indeks[,1]) ##### indices of independent variables (for kernel regression) start from the first column in indeks.csv 
 
 x = matrix(ncol=length(indeksx),nrow=nrow(y))
 
@@ -369,7 +369,7 @@ for (i in 1:length(indeksx))
 {
  for (j in 1:nrow(y))
  {
-   x[j,i] = mydata[j,indeksx[i]+3] #### variabel respon dimulai dari kolom ke-4 di file datacoba.csv
+   x[j,i] = mydata[j,indeksx[i]+3] #### the indices of independent variables (for kernel and spline regressions) start from the 4-th column in datacoba.csv
  }
 }
 
@@ -380,29 +380,29 @@ x[1,1]
 x[1,2]
 
 ###########################################################################################
-########### (4.1) Pencarian bandwidth optimum untuk Regresi Kernel ########################
+########### (4.1) Optimal bandwidth search for Kernel Regression ########################
 ###########################################################################################
 
-#### Inisialisasi input PSO untuk Regresi Kernel##############
+#### PSO main function input initialization for Kernel regression##############
 
 
-N = 50; ### Banyaknya calon solusi awal
-itermaks = 1; ### banyaknya iterasi maksimum
-nvar = 1; ### diisi dengan banyaknya variabel yang akan diproses
+N = 50; ### the number of initial solutions
+itermaks = 1; ### number of iterations
+nvar = 1; ### number of variables (dependent + independent)
 
-#### parameter fiks #######
+#### fixed parameter #######
 D = matrix(0,nrow(x),nrow(x))
 degree = 1
-nknot = 1 ### nknot = nvar untuk kasus regresi kernel 
+nknot = 1 ### nknot = nvar for kernel regression 
 ############################
 
-band = matrix(0, ncol = nrow(indeks), nrow = 1) ######### Matriks tempat menyimpan bandwidth
-nilaigcv = matrix(0, ncol = nrow(indeks), nrow = 1) ###### Matriks tempat menyimpan GCV
+band = matrix(0, ncol = nrow(indeks), nrow = 1) ######### bandwidths matrix
+nilaigcv = matrix(0, ncol = nrow(indeks), nrow = 1) ###### GCV values matrix
 
- for (indvar in 1:nrow(indeks)) ##### awal iterasi utama pencarian bandwidth untuk masing-masing variabel 
+ for (indvar in 1:nrow(indeks)) 
  {
 
- ###### batas atas dan bawah masing-masing bandwidth ########
+ ###### upper and lower bounds for each bandwidth ########
 
  batasvar = matrix(0, nrow = nvar, ncol = 2)
 
@@ -414,33 +414,25 @@ nilaigcv = matrix(0, ncol = nrow(indeks), nrow = 1) ###### Matriks tempat menyim
  ###############################################################
 
  xin = x[,indvar]
- bandplusgcv = pso(N,itermaks,nvar,gcv,y,xin,batasvar,D,degree,nknot); ######## pencarian bandwidth optimum dengan PSO (Ingat: nknot = nvar = 1)
+ bandplusgcv = pso(N,itermaks,nvar,gcv,y,xin,batasvar,D,degree,nknot); ######## PSO search (note: nknot = nvar = 1 for kernel regression)
 
  band[indvar] = bandplusgcv[1]
  nilaigcv[indvar] = bandplusgcv[2]
  
- #bandplusgcv[1:length(bandplusgcv)-1]
- #print(gcv(y,xin,bandplusgcv[1:length(bandplusgcv)-1]))
-
- #print(gcv(y,xin,5))
- #print(kernel((xin[1]-xin[2])/5))
- #print(nadwatson(xin[1],xin[2],xin,5))
- #print(euclidnorm(xin))
-} ###### akhir iterasi pencarian bandwidth
+} ###### the end of bandwidth search
 
 print(band)
-#print(nilaigcv) #### yang diprint adalah GCV bersama di bagian bawah
 
-######### Pembentukan matriks D(alpha) ###############
+######### D(alpha) matrix ###############
 
 n = length(y)
 Dalpha = matrix(0, nrow = n, ncol = n)
 
 for (indvar in 1:nrow(indeks))
 {
- D = matrix(0, ncol = n, nrow = n) ### Inisialisasi matriks D(h)
+ D = matrix(0, ncol = n, nrow = n) 
  
- for (k in 1:n) ##### pengisian entri matriks D(h) dengan fungsi W dari estimator Nadaraya-Watson
+ for (k in 1:n) 
  {
    for (l in 1:n)
    {
@@ -451,11 +443,7 @@ for (indvar in 1:nrow(indeks))
 }
 
 
-###### Coba program jalan/tidak :D ############
-
-print(Dalpha)
-
-######## cetak GCV ############
+######## print GCV values ############
 
 ndata = nrow(Dalpha)
 I = diag(ndata)
@@ -466,26 +454,21 @@ gcvkernel = HA/HB
 print(gcvkernel)
 
 
-#knot1 = c(3,4.5,5.5)
-#knot2 = c(310,390,425)
-#knot = rbind(knot1,knot2)
-#h = 3
-#gcvmix(y,x,h,Dalpha,1,knot)
 
 #################################################
 
 ########################################################################
-############ (4.2) Pencarian Knot optimum untuk Spline truncated #############
+############ (4.2) optimal Knots for Spline truncated regression #############
 ########################################################################
 
-###### parameter fiks ########
+###### fixed parameters for 'appropriate' input in PSO main function ########
 
-h = 3
+h = 3 ### h = number of independent variables in the case of spline truncated regression
 ###############################
 
-###### Ambil data t ################
+###### Indices for independent variables ################
 
-indekst = removezero(indeks[,2]) ##### indeks variabel x ada di kolom ke-2 di file indeks.csv
+indekst = removezero(indeks[,2]) ##### indices of independent variables (for spline truncated regression) start from the 2nd column in indeks.csv
 
 t = matrix(ncol=length(indekst),nrow=nrow(y))
 
@@ -493,16 +476,16 @@ for (i in 1:length(indekst))
 {
  for (j in 1:nrow(y))
  {
-   t[j,i] = mydata[j,indekst[i]+3] #### variabel respon dimulai dari kolom ke-4 di file datacoba.csv
+   t[j,i] = mydata[j,indekst[i]+3] #### the indices of independent variables (for kernel and spline regressions) start from the 4-th column datacoba.csv
  }
 }
 
 #print(dim(t))
 #print(t)
 
-############ Range nilai variabel ########
+############ Collects lower and upper bounds for each independent variable ########
 
-nvar = length(indekst) ##### banyaknya variabel
+nvar = length(indekst) 
 batasvar = matrix(0,nrow = nvar, ncol = 2)
 
 for (i in 1:nvar)
@@ -511,21 +494,21 @@ for (i in 1:nvar)
 	batasvar[i,2] = max(t[,i])
 }
 
-#### Inisialisasi input PSO untuk Spline ##############
+#### PSO main function initial input for Spline ##############
 
 
-N = 50; ### Banyaknya calon solusi awal
-itermaks = 10; ### banyaknya iterasi maksimum
+N = 50; ### the number of initial solutions
+itermaks = 10; ### number of iterations
 
-degree = 3
-nknot = 4
+degree = 3 ### the degree of polynomial in spline
+nknot = 4 ## number of knots
 
-knotplusmgcv = pso(N,itermaks,nvar,gcvmix,y,t,batasvar,Dalpha,degree,nknot); ######## pencarian bandwidth optimum dengan PSO 
+knotplusmgcv = pso(N,itermaks,nvar,gcvmix,y,t,batasvar,Dalpha,degree,nknot); ######## PSO search 
 
 knotplusmgcv
 
 
-################# Cetak data prediksi oleh model #############
+################# print predicted data #############
 
 knotakhir = knotplusmgcv[1:(length(knotplusmgcv)-1)]
 
@@ -533,9 +516,9 @@ n = nrow(t)
 nmat = ncol(t)
 nknot = length(knotakhir)/nmat
 m = degree + 1 + nknot
-Gk = matrix(0,nrow = n, ncol = m) ###### inisialisasi matriks G(k)
+Gk = matrix(0,nrow = n, ncol = m) 
  
- for (indcol in 1:m) ###### menghitung G(k_1)
+ for (indcol in 1:m) 
  {
 	if (indcol <= degree+1)
 	{
@@ -553,10 +536,10 @@ Gk = matrix(0,nrow = n, ncol = m) ###### inisialisasi matriks G(k)
 	}
  }	
  
- for (indvar in 2:nmat) #### menghitung G(k_2),...,G(k_p) dan digabung jadi G(k)
+ for (indvar in 2:nmat) 
  {
 	Gtemp = matrix(0, nrow = n, ncol = m)
-	for (indcol in 1:m) ###### menghitung G(k_1)
+	for (indcol in 1:m) 
  	{
 		if (indcol <= (degree+1))
 		{
@@ -577,8 +560,7 @@ Gk = matrix(0,nrow = n, ncol = m) ###### inisialisasi matriks G(k)
 
  	Gk = cbind(Gk,Gtemp)	
  }
- #print(Gk)
- #print(t(Gk)%*%Gk)
+ 
  I2 = diag(nrow(Dalpha))
  K = Gk%*%pinv((t(Gk)%*%Gk))%*%t(Gk)%*%(I2-Dalpha)
  M = K + Dalpha
@@ -587,5 +569,9 @@ ypred = M%*%y
 
 print(ypred)
 
-#plot(y,type="b", col='red')
-#plot(ypred, col = 'blue', add = TRUE)
+#####################################
+#### plot predicted vs original #####
+####################################
+
+plot(y,type="b", col='red')
+plot(ypred, col = 'blue', add = TRUE)
